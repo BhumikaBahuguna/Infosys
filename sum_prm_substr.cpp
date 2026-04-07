4e5a7b296d2b7be2ffec9ca9d26671328855e348
BHUMIKA BAHUGUNA <158489676+BhumikaBahuguna@users.noreply.github.com>
2026-04-07 14:06:42 +0530

    Add files via upload

---
+++ sum_prm_substr.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+    bool isPrime(int x) {
+        if(x <= 1) return false;
+        if(x == 2) return true;
+        if(x % 2 == 0) return false;
+        for(int i = 3; i * i <= x; i += 2) {
+            if(x % i == 0) return false;
+        }
+        return true;
+    }
+    long long sumOfLargestPrimes(string s) {
+        int n = s.size();
+        unordered_set<int> primes;
+        for(int i = 0; i < n; i++) {
+            long long num = 0;
+            for(int j = i; j < n; j++) {
+                num = num * 10 + (s[j] - '0');
+                if(isPrime(num)) {
+                    primes.insert(num);
+                }
+            }
+        }
+        vector<int> v(primes.begin(), primes.end());
+        sort(v.begin(), v.end(), greater<int>());
+        long long sum = 0;
+        for(int i = 0; i < min(3, (int)v.size()); i++) {
+            sum += v[i];
+        }
+        return sum;
+    }
+int main(){
+    string s;
+    cin >> s;
+    cout << sumOfLargestPrimes(s) << endl;  
+}
