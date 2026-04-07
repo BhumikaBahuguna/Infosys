4edaa1649a6dbc1d081e6316bbc807dd5e3de92c
BHUMIKA BAHUGUNA <158489676+BhumikaBahuguna@users.noreply.github.com>
2026-04-07 14:34:47 +0530

    Update sum_prm_substr.cpp

--- sum_prm_substr.cpp
+++ sum_prm_substr.cpp
@@ -1,17 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
-    bool isPrime(int x) {
+    bool isPrime(long long x) { 
         if(x <= 1) return false;
         if(x == 2) return true;
         if(x % 2 == 0) return false;
-        for(int i = 3; i * i <= x; i += 2) {
+        for(long long i = 3; i * i <= x; i += 2) {
             if(x % i == 0) return false;
         }
         return true;
     }
     long long sumOfLargestPrimes(string s) {
-        int n = s.size();
-        unordered_set<int> primes;
+        int n = s.size();   
+        unordered_set<long long> primes; 
         for(int i = 0; i < n; i++) {
             long long num = 0;
             for(int j = i; j < n; j++) {
@@ -21,8 +21,8 @@ using namespace std;
                 }
             }
         }
-        vector<int> v(primes.begin(), primes.end());
-        sort(v.begin(), v.end(), greater<int>());
+        vector<long long> v(primes.begin(), primes.end()); 
+        sort(v.begin(), v.end(), greater<long long>());
         long long sum = 0;
         for(int i = 0; i < min(3, (int)v.size()); i++) {
             sum += v[i];
