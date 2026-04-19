70f081ca0b2ca3eb5bc9cd128cb95539de3e9d93
BHUMIKA BAHUGUNA <158489676+BhumikaBahuguna@users.noreply.github.com>
2026-04-20 00:30:21 +0530

    Update segtree_sum.cpp

--- segtree_sum.cpp
+++ segtree_sum.cpp
@@ -1,123 +1,60 @@
-#include <iostream>
-#include <cmath>
+#include<bits/stdc++.h>
 using namespace std;
-
-// Calculate size of segment tree
-int calculate_size(int n)
-{
-    int x = ceil(log2(n));
-    int size = 2 * pow(2, x) - 1;
-    return size;
-}
-
-// Initialize segment tree
-int* init(int size)
-{
-    int *temp = new int[size];
-    return temp;
-}
-
-// Build tree
-void buildTree(int *segment_tree, int *arr, int sl, int sr, int i)
-{
-    if (sl == sr)
-    {
-        segment_tree[i] = arr[sl];
-        return;
+vector<int>arr;
+vector<int>seg;
+void buildseg(int node,int l,int r){
+    if(l==r){
+        seg[node]=arr[l];
+        return ;
     }
-
-    int mid = sl + (sr - sl) / 2;
-
-    buildTree(segment_tree, arr, sl, mid, 2 * i + 1);
-    buildTree(segment_tree, arr, mid + 1, sr, 2 * i + 2);
-
-    segment_tree[i] = segment_tree[2 * i + 1] + segment_tree[2 * i + 2];
+    int mid=(l+r)/2;
+    buildseg(2*node+1,l,mid);
+    buildseg(2*node+2,mid+1,r);
+    seg[node]=seg[2*node+1]+seg[2*node+2];
 }
-
-// Update utility
-void update_utils(int *segment_tree, int sl, int sr, int cur, int index, int key, int changeValue)
-{
-    // If index is out of range
-    if (index < sl || index > sr)
-        return;
-
-    // Update current node
-    segment_tree[cur] += changeValue;
-
-    if (sl != sr)
+int query(int node,int l,int r,int ql,int qr){
+    if(qr<l || ql>r) return 0;
+    if(ql<=l && r<=qr) return seg[node];
+    int mid=(l+r)/2;
+    return query(2*node+1,l,mid,ql,qr)+query(2*node+2,mid+1,r,ql,qr);
+}
+void update(int node,int l,int r,int idx,int val){
+    if(l==r)
     {
-        int mid = sl + (sr - sl) / 2;
-
-        if (index <= mid)
-            update_utils(segment_tree, sl, mid, 2 * cur + 1, index, key, changeValue);
-        else
-            update_utils(segment_tree, mid + 1, sr, 2 * cur + 2, index, key, changeValue);
+        seg[node]=val;
+        arr[idx]=val;
+        return ;
     }
+    int mid=(l+r)/2;
+    if(idx<=mid)
+        update(2*node+1,l,mid,idx,val);
+    else
+        update(2*node+2,mid+1,r,idx,val);
+    seg[node]=seg[2*node+1]+seg[2*node+2];
 }
-
-// Update function
-void update(int *arr, int *segment_tree, int n, int index, int key)
-{
-    int changeValue = key - arr[index];
-    arr[index] = key;
-
-    update_utils(segment_tree, 0, n - 1, 0, index, key, changeValue);
-}
-
-// Range sum query
-int calculateSum(int *arr, int *segment_tree, int sl, int sr, int index, int ql, int qr)
-{
-    // No overlap
-    if (sr < ql || sl > qr)
-        return 0;
-
-    // Complete overlap
-    if (sl >= ql && sr <= qr)
-        return segment_tree[index];
-
-    int mid = sl + (sr - sl) / 2;
-
-    return calculateSum(arr, segment_tree, sl, mid, 2 * index + 1, ql, qr) +
-           calculateSum(arr, segment_tree, mid + 1, sr, 2 * index + 2, ql, qr);
-}
-
-// Driver
-int main()
-{
-    int n, *arr, Q, index, key, l, r, type;
-
-    cin >> n;
-
-    arr = new int[n];
-    for (int i = 0; i < n; i++)
-        cin >> arr[i];
-
-    int segmentSize = calculate_size(n);
-    int *segment_tree = init(segmentSize);
-
-    buildTree(segment_tree, arr, 0, n - 1, 0);
-
-    cin >> Q;
-
-    while (Q--)
-    {
-        cin >> type;
-
-        switch (type)
-        {
-        case 1:
-            cin >> index >> key;
-            update(arr, segment_tree, n, index, key);
-            break;
-
-        case 2:
-            cin >> l >> r;
-            cout << "Sum = " 
-                 << calculateSum(arr, segment_tree, 0, n - 1, 0, l, r) 
-                 << endl;
-            break;
+int main(){
+    int n;
+    cin>>n;
+    arr.resize(n);
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    seg.resize(4*n);
+    buildseg(0,0,n-1);
+    int q;
+    cin>>q;
+    while(q--){
+        int type;
+        cin>>type;
+        if(type==1){
+            int l,r;
+            cin>>l>>r;
+            cout<<query(0,0,n-1,l,r);
+        }else if(type==2){
+            int idx,val;
+            cin>>idx>>val;
+            update(0,0,n-1,idx,val);
         }
     }
-
     return 0;
 }
