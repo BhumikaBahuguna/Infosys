f6668cdb1b25e8ba739d9ccfd2234f49d69ddb30
BHUMIKA BAHUGUNA <158489676+BhumikaBahuguna@users.noreply.github.com>
2026-04-20 23:32:04 +0530

    Add files via upload

---
+++ frac_knapsack.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct Item {
+    int value;
+    int weight;
+};
+class Solution {
+public:
+    bool static comp(Item a, Item b) {
+        double r1 = (double) a.value / (double) a.weight;
+        double r2 = (double) b.value / (double) b.weight;
+        return r1 > r2;  
+    }
+    double fractionalKnapsack(int W, Item arr[], int n) {
+        sort(arr, arr + n, comp);
+        int curWeight = 0; 
+        double finalvalue = 0.0;  
+        for (int i = 0; i < n; i++) {
+            if (curWeight + arr[i].weight <= W) {
+                curWeight += arr[i].weight;
+                finalvalue += arr[i].value;  
+            } else {
+                int remain = W - curWeight;
+                finalvalue += (arr[i].value / (double) arr[i].weight) * (double) remain;
+                break;  
+            }
+        }
+        return finalvalue;  
+    }
+};
+
+int main() {
+    int n, W;
+    cin >> n >> W;
+    Item arr[n];
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i].value >> arr[i].weight;
+    }
+    Solution ob;
+    cout << ob.fractionalKnapsack(W, arr, n) << endl;
+    return 0;
+}
