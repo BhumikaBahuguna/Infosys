889fbf07602ed77ea518a2e4f6b84f91dab64ea4
BHUMIKA BAHUGUNA <158489676+BhumikaBahuguna@users.noreply.github.com>
2026-04-21 08:47:24 +0530

    Add files via upload

---
+++ valid_paren.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool checkValidString(string s) {
+    int low=0,high=0;
+        for(char c:s){
+            if(c=='('){
+                low++;
+                high++;   
+            }else if (c==')'){
+                low--;
+                high--;
+            }else{
+                low--;
+                high++;
+            }
+            if(high<0)
+                return false;
+            low=max(low,0);
+    }
+    return low==0;
+}
+int main(){
+    string s;
+    cin>>s;
+    cout<<checkValidString(s);
+}
